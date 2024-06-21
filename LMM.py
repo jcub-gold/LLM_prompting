23c7fa62b6cb1a8381f067206555058a85b96dbd
Jacob Goldberg <jacobgoldbergredsox@gmail.com>
2024-06-21 14:35:04 -0700

    working inheritence

--- LMM.py
+++ LMM.py
@@ -5,11 +5,35 @@ from openai import OpenAI
 
 load_dotenv()
 
-class Gemini:
+class LLM:
     def __init__(self, **kwargs):
-        self.api_key = os.getenv("GOOGLE_API_KEY")
+        allowed_attributes = {'model': None}
+        
+        for key, value in kwargs.items():
+            if key in allowed_attributes:
+                setattr(self, key, value)
+            else:
+                raise AttributeError(f"Invalid attribute: {key}")
+            
+        for key in allowed_attributes:
+            if not hasattr(self, key):
+                setattr(self, key, allowed_attributes[key])
+    
+    def set_prompt(self, **kwargs):
+        allowed_attributes = {'prompt': None}
+        
+        for key, value in kwargs.items():
+            if key in allowed_attributes:
+                setattr(self, key, value)
+            else:
+                raise AttributeError(f"Invalid attribute: {key}")
+            
+        for key in allowed_attributes:
+            if not hasattr(self, key):
+                setattr(self, key, allowed_attributes[key])
 
-        allowed_attributes = {'model': 'gemini-pro'}
+    def set_input_file(self, **kwargs):
+        allowed_attributes = {'input_file': None}
         
         for key, value in kwargs.items():
             if key in allowed_attributes:
@@ -22,59 +46,86 @@ class Gemini:
                 setattr(self, key, allowed_attributes[key])
 
     def generate_response(self, **kwargs):
+        pass
+
+    """
+    FUCNTION: prompt_pairs_batch_from_file
+    ---------
+    PARAMETERS: 
+        input_file - the name of a txt file formated with one prompt per line
+    -----------
+    DESCRIPTION:
+    This function read prompts from a file and uses the specified model instance
+    to generate a response from the AI model. The prompts are expected to be in pairs,
+    meaning every two prompts relate to each other. This function has no return value
+    but will update the contents of the output file with the contents of the prompts
+    responses.    
+    """
+    def prompt_pairs_batch_from_file(self, **kwargs):
+        line_num = 0
+        self.set_input_file(**kwargs)
+
+        with open(self.input_file, 'r') as input:
+            print('reading file...')
+            for line in input:
+                line_num = line_num + 1
+                i_prompt = line.strip()
+                response = self.generate_response(prompt = i_prompt)
+                flag = 'a'
+                if (line_num == 1):
+                    flag = 'w'
+                output_file = self.get_model() + '_output.txt'
+                with open(output_file, flag) as output:
+                    if (line_num % 2 == 1):
+                        output.write("Prompt #" + str((line_num + 1) // 2) + ": ")
+                    else:
+                        output.write("Folow-up prompt #" + str(line_num // 2) + ": ")
+                    output.write(i_prompt + "\n\nResponse: " + response + "\n\n")
+                    if (line_num % 2 == 0):
+                        output.write("\n")
+            print('done')
+    
+
+class Gemini(LLM):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.api_key = os.getenv("GOOGLE_API_KEY")
+
+    def generate_response(self, **kwargs):
+        super().set_prompt(**kwargs)
         model = genai.GenerativeModel(self.model)
         genai.configure(api_key=self.api_key)
-
-        allowed_attributes = {'prompt': ''}
-        
-        for key, value in kwargs.items():
-            if key in allowed_attributes:
-                setattr(self, key, value)
-            else:
-                raise AttributeError(f"Invalid attribute: {key}")
-            
-        for key in allowed_attributes:
-            if not hasattr(self, key):
-                setattr(self, key, allowed_attributes[key])
-
         response = model.generate_content(self.prompt)
         return response.text
 
     def get_model(self):
         return self.model
+    
+    def set_input_file(self, **kwargs):
+        return super().set_input_file(**kwargs)
+    
+    def prompt_pairs_batch_from_file(self, **kwargs):
+        return super().prompt_pairs_batch_from_file(**kwargs)
 
-class GPT:
+class GPT(LLM):
     def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.api_key = os.getenv("OPENAI_API_KEY")
         self.client = OpenAI(api_key = self.api_key)
-        
-        allowed_attributes = {'model': 'gpt-3.5-turbo'}
-        
-        for key, value in kwargs.items():
-            if key in allowed_attributes:
-                setattr(self, key, value)
-            else:
-                raise AttributeError(f"Invalid attribute: {key}")
-            
-        for key in allowed_attributes:
-            if not hasattr(self, key):
-                setattr(self, key, allowed_attributes[key])
 
     def generate_response(self, **kwargs):
-        allowed_attributes = {'prompt': ''}
-        
-        for key, value in kwargs.items():
-            if key in allowed_attributes:
-                setattr(self, key, value)
-            else:
-                raise AttributeError(f"Invalid attribute: {key}")
-            
-        for key in allowed_attributes:
-            if not hasattr(self, key):
-                setattr(self, key, allowed_attributes[key])
-                
+        super().set_prompt(**kwargs)
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[{"role": "user", "content": self.prompt}])
         return response.choices[0].message.content
+    
+    def get_model(self):
+        return self.model
+    
+    def set_input_file(self, **kwargs):
+        return super().set_input_file(**kwargs)
+
+    def prompt_pairs_batch_from_file(self, **kwargs):
+        return super().prompt_pairs_batch_from_file(**kwargs)
 
--- run.py
+++ run.py
@@ -1,44 +1,6 @@
 from LMM import GPT
 from LMM import Gemini
 
-"""
-FUCNTION: prompt_pairs_batch_from_file
----------
-PARAMETERS: 
-    input_file - the name of a txt file formated with one prompt per line
-    output_file - the name of txt file; content of this file will be overwritten
-                    with the prompts and AI responses
-    model_instance - an initialized instance of one of the models from the LMM  file
------------
-DESCRIPTION:
-This function read prompts from a file and uses the specified model instance
-to generate a response from the AI model. The prompts are expected to be in pairs,
-meaning every two prompts relate to each other. This function has no return value
-but will update the contents of the output file with the contents of the prompts
-responses.    
-"""
-def prompt_pairs_batch_from_file(input_file, output_file, model_instance):
-    line_num = 0
-
-    with open(input_file, 'r') as input:
-        print('reading file...')
-        for line in input:
-            line_num = line_num + 1
-            i_prompt = line.strip()
-            response = model_instance.generate_response(prompt = i_prompt)
-            flag = 'a'
-            if (line_num == 1):
-                flag = 'w'
-            with open(output_file, flag) as output:
-                if (line_num % 2 == 1):
-                    output.write("Prompt #" + str((line_num + 1) // 2) + ": ")
-                else:
-                    output.write("Folow-up prompt #" + str(line_num // 2) + ": ")
-                output.write(i_prompt + "\n\nResponse: " + response + "\n\n")
-                if (line_num % 2 == 0):
-                    output.write("\n")
-        print('done')
-
 def main():
     LLM = input('Would you like to use ChatGPT(1) or Gemini(2)?\n')
     i_model = input('What model would you like to use?\n')
@@ -46,9 +8,8 @@ def main():
         LLM = GPT(model = i_model)
     else:
         LLM = Gemini(model = i_model)
-    input_file = input('Please enter the name of your input file (including the .txt):\n')
-    output_file = input('Please eneter the name of your ouput file (including the .txt):\n')
-    prompt_pairs_batch_from_file(input_file, output_file, LLM)
+    i_input_file = input('Please enter the name of your input file (including the .txt):\n')
+    LLM.prompt_pairs_batch_from_file(input_file = i_input_file)
 
 if __name__ == "__main__":
     main()
